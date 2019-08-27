f60647ed29b280482ab4632d788a0d5cb8ab8700
dimik <dimik@yandex.ru>
2019-08-27 12:31:51 +0700

    Updates

---
+++ precache-manifest.a5766ffe389dbb6424b4013c2f2c06f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76b23f30c225da37eb362db8d8f873fe",
+    "url": "/adform/index.html"
+  },
+  {
+    "revision": "e2a52f9c68c08d37971e",
+    "url": "/adform/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "495aa93d31f5d3b10d8f",
+    "url": "/adform/static/js/2.958d9117.chunk.js"
+  },
+  {
+    "revision": "e2a52f9c68c08d37971e",
+    "url": "/adform/static/js/main.51860aa4.chunk.js"
+  },
+  {
+    "revision": "b32ed5dc0b76a27014e7",
+    "url": "/adform/static/js/runtime~main.4f8b1b0b.js"
+  }
+]);
